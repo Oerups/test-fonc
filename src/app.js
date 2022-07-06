86b102d80fad9dff137d1103c9c3c20616647bcd
tdargent1 <tdargent1@gmail.com>
2022-07-06 10:53:58 +0200

    init

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const dotenv = require("dotenv");
+const express = require("express");
+const cors = require("cors");
+
+// const databaseConfig = require("./config/database.config");
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(cors());
+
+dotenv.config();
+
+// databaseConfig.MongoDB().catch((err) => console.log(err));
+
+app.use("/", () => console.log("Server running"));
+
+app.listen(process.env.PORT || 3000, () => console.log("server listening"));
